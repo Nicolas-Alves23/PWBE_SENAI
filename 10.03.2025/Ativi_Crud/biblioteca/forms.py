20cd0774a6767e69ab196ae350031dc69a24a486
nicolasalvesss <nicolasvilelabarros@gmail.com>
2025-03-11 11:26:13 -0300

    ...

--- 10.03.2025/Ativi_Crud/biblioteca/forms.py
+++ 10.03.2025/Ativi_Crud/biblioteca/forms.py
@@ -4,4 +4,4 @@ from .models import Biblioteca
 class Bibliotecaforms(forms.ModelForm):
     class Meta:
         model = Biblioteca
-        fields = ['titulo','autor','ano_pubi','descricao']
+        fields = '__all__'
--- 10.03.2025/Ativi_Crud/biblioteca/views.py
+++ 10.03.2025/Ativi_Crud/biblioteca/views.py
@@ -3,8 +3,8 @@ from .models import Biblioteca
 from .forms import Bibliotecaforms
 
 def listar_livros(request):
-    livro = Biblioteca.objects.all()
-    return render(request, 'biblioteca/listar_livros.html',{'biblioteca': biblioteca})
+    livros = Biblioteca.objects.all()
+    return render(request, 'listar_livros.html',{'livros' : livros})
 
 def cadastrar_livro(request):
     if request.method == 'POST':
@@ -14,10 +14,10 @@ def cadastrar_livro(request):
             return redirect('listar_livros')
     else:
         form = Bibliotecaforms()
-    return render(request,'biblioteca/cadastrar.html',{'form': form})
+    return render(request,'cadastrar_livro.html',{'form': form})
 
 def atualizar_livros(request,pk):
-    livro = get_object_or_404(Biblioteca, pk = pk)
+    livro = get_object_or_404(Biblioteca, pk=pk)
     if request.method == 'POST':
         form = Bibliotecaforms(request.POST, instance = livro)
         if form.is_valid():
@@ -25,11 +25,29 @@ def atualizar_livros(request,pk):
             return redirect('listar_livros')
     else:
         form = Bibliotecaforms(instance=livro)
-    return render(request, 'biblioteca/atualizar_livro.html', {'form':form, 'livro':livro})
+    return render(request, 'atualizar_livro.html', {'form':form, 'livro':livro})
 
 def excluir_livro(request,pk):
     livro = get_object_or_404(Biblioteca, pk=pk)
     if request.method == 'POST':
         livro.delete()
         return redirect('listar_livros')
-    return render(request,'biblioteca/excluir_livro.html', {'livro':livro})
+    return render(request,'excluir_livro.html', {'livro':livro})
+
+
+
+def lista_livro(request):
+    titulo = request.GET.get('titulo', None)
+    ano = request.GET.get('ano_pubi', None)
+    autor = request.GET.get('autor', None)
+
+    # Inicia o queryset com todos os livro
+    livro = Biblioteca.objects.all()
+
+    # Aplica o filtro para cada campo, se o valor for fornecido
+    if titulo:
+        livro = livro.filter(titulo__icontains=titulo) | livro.filter(ano=ano) | livro.filter(autor__icontains=autor) # 'icontains' para pesquisa insensível a maiúsculas/minúsculas
+
+
+    # Retorna os livro filtrados para a página
+    return render(request, 'listar_livro.html', {'livro': livro})
